ad815acae48e5d8a7a31cc25b1cc144804f63c0d
Denzel18rb <74129875rb@gmail.com>
2023-06-22 18:59:22 -0700

    Program that prints the alphabet ... and fix command task 2 error betty corrected

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,12 @@
 int main(void)
 {
 char letter;
+char letter_m;
 
 for (letter = 'a'; letter <= 'z'; letter++)
 putchar(letter);
+for (letter_m = 'A'; letter_m <= 'Z'; letter_m++)
+putchar(letter_m);
 putchar ('\n');
 
 return (0);
