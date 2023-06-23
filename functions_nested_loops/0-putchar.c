6b83aba5870104ae5a960e839201907498012e04
Denzel18rb <74129875rb@gmail.com>
2023-06-23 16:46:13 -0700

    Fix archive main.h

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**  
+/**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 printf("_putchar\n");
-return 0;
+return (0);
 }
