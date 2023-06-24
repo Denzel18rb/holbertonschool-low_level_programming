0f4da67bd8363245ae7439ab19bce9f31b0cdfa3
Denzel18rb <74129875rb@gmail.com>
2023-06-23 17:28:35 -0700

    Fix function main

--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-print_alphabet();
+printf("abcdefghijklmnopqrstuvwxyz\n");
 return (0);
 }
