925edd27fc612c1709853171f3c9d493fba4bc2f
Denzel18rb <135359331+Denzel18rb@users.noreply.github.com>
2023-06-30 16:57:45 -0500

    :D main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,18 +1,17 @@
-#ifndef main_h
-#define main_h
-/**
- * main - check the code
- *
- * Return: 0 (Success)
- */
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-int _isupper(int c);
-#endif
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+#endif /* MAIN_H */
