29d49a2c6f0f6224ab786e44e6b37cb41bd670c0
Denzel18rb <74129875rb@gmail.com>
2023-07-03 19:14:03 -0700

    Define a new type struct dog with the following elements

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _dog_h_
+#define _dog_h_
+#include <stdio.h>
+
+/**
+ * Struct dog - function containing a structure
+ * @name: Name the dog
+ * @age: How old is the dog
+ * @owner: Owner the dog
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+
+#endif /* dog.h */
